7a1ae4ef47da140823d7eba6b4246ffd34b5e398
Martin Gansler <martin.gansler@codecentric.de>
2019-07-03 09:36:29 +0200

    Make responsive to browser resize

--- src/Grid/Grid.spec.tsx
+++ src/Grid/Grid.spec.tsx
@@ -4,16 +4,25 @@ import { Grid } from "./Grid"
 
 const ItemMock: React.FC<{ a: number }> = ({ a }) => <div>{a}</div>
 
-it("should render as expected when no items given", () => {
-  const container = document.createElement("div")
-  ReactDOM.render(<Grid Item={ItemMock} items={[]}/>, container)
+let container: HTMLDivElement
 
-  expect(container.innerHTML).toBe("<div class=\"grid\"></div>")
+beforeEach(() => {
+  container = document.createElement("div")
 })
 
-it("should render as expected when two items given", () => {
+afterEach(() => {
+  ReactDOM.unmountComponentAtNode(container)
+})
+
+it("should render empty grid", () => {
+  ReactDOM.render(<Grid Item={ItemMock} items={[]}/>, container)
+
+  expect(container.innerHTML).toBe("<div class=\"grid\" style=\"grid-template-rows: repeat(0, minmax(400px, 1fr);\"></div>")
+})
+
+it("should render two items", () => {
   const container = document.createElement("div")
   ReactDOM.render(<Grid Item={ItemMock} items={[{ a: 1 }, { a: 2 }]}/>, container)
 
-  expect(container.innerHTML).toBe("<div class=\"grid\"><div>1</div><div>2</div></div>")
+  expect(container.innerHTML).toBe("<div class=\"grid\" style=\"grid-template-rows: repeat(2, minmax(400px, 1fr);\"><div>1</div><div>2</div></div>")
 })
--- src/Grid/Grid.tsx
+++ src/Grid/Grid.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { useEffect, useRef, useState } from "react"
 import "./grid.css"
 
 interface GridProps {
@@ -7,8 +8,28 @@ interface GridProps {
 }
 
 export const Grid: React.FC<GridProps> = ({ items, Item }) => {
+  const gridRef = useRef<HTMLDivElement>(null)
+  const [rowCount, setRowCount] = useState<number>(items.length)
+
+  const handleResize = () => {
+    if (gridRef.current !== null && items.length > 0) {
+      const { clientWidth, childNodes } = gridRef.current
+      const columnCount = Math.floor(clientWidth / (childNodes.item(0) as HTMLDivElement).clientWidth)
+      setRowCount(Math.ceil(items.length / columnCount))
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize)
+    handleResize()
+
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+  })
+
   return (
-    <div className={"grid"}>
+    <div className={"grid"} ref={gridRef} style={{ gridTemplateRows: `repeat(${rowCount}, minmax(400px, 1fr)` }}>
       {items.map((item, index) => <Item
         key={index}
         {...item}
