46febc3ebe0c9a0b38aa68a4134878421d6fa774
Martin Gansler <martin.gansler@codecentric.de>
2019-07-15 16:38:25 +0200

    Combine util functions into single file

--- src/isUpdateRequired.ts
+++ src/isUpdateRequired.ts
@@ -1,5 +1,19 @@
 import { GridState } from "./types"
-import { areVisibleItemsEqual } from "./areVisibleItemsEqual"
+
+// Compares the first and last element of the given arrays
+const areVisibleItemsEqual = (lastVisibleItems: number[], newVisibleItems: number[]) => {
+  if (lastVisibleItems.length !== newVisibleItems.length) {
+    return false
+  }
+  const length = lastVisibleItems.length
+
+  if (length === 0) {
+    // both empty
+    return true
+  }
+
+  return lastVisibleItems[0] === newVisibleItems[0] && lastVisibleItems[length - 1] === newVisibleItems[length - 1]
+}
 
 export const isUpdateRequired = (oldGridState: GridState, newGridState: GridState): boolean => {
   const { rows: currentRows, columns: currentColumns, visibleItems: currentVisibleItems } = oldGridState
