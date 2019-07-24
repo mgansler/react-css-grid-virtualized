24776cd3be4cef14b70b490cec1789bee2d8e4cc
Martin Gansler <martin.gansler@codecentric.de>
2019-07-24 10:50:44 +0200

    Remove not needed code from blog directory

--- blog/src/Kitten.tsx
+++ blog/src/Kitten.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import "./item.css"
+import "./kitten.css"
 import { GridPosition } from "./types"
 
 export interface KittenProps {
@@ -9,7 +9,7 @@ export interface KittenProps {
 
 export const Kitten: React.FC<KittenProps & GridPosition> = ({ width, height, gridColumnStart, gridRowStart }) => {
   return <div
-    className={"item"}
+    className={"kitten"}
     style={{
       backgroundImage: `url(https://placekitten.com/g/${width}/${height})`,
       gridRowStart,
